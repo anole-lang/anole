efa425c495f04aa3b7e02ddf546745c2a8653aa5
mu001999 <mu001999@outlook.com>
2018-05-28 23:00:11 +0800

    init test

---
+++ test/TestLexicalAnalyzer.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../ice/include/LexicalAnalyzer.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace Ice;
+
+TEST_CLASS(TestLexicalAnalyzer)
+{
+public:
+		
+	TEST_METHOD(TestLexerIdentifier)
+	{
+		std::string code = R"coldice()coldice";
+	}
+
+};
