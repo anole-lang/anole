a02a667033e8de2a484c80fc1b0efb2b17af596e
mu001999 <mu001999@outlook.com>
2019-11-23 23:23:15 +0800

    update

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -6,4 +6,159 @@ void BlockExpr::codegen(Code &code)
 {
 
 }
+
+void NoneExpr::codegen(Code &code)
+{
+
+}
+
+void IntegerExpr::codegen(Code &code)
+{
+
+}
+
+void FloatExpr::codegen(Code &code)
+{
+
+}
+
+void BoolExpr::codegen(Code &code)
+{
+
+}
+
+void StringExpr::codegen(Code &code)
+{
+
+}
+
+void IdentifierExpr::codegen(Code &code)
+{
+
+}
+
+void ParenOperatorExpr::codegen(Code &code)
+{
+
+}
+
+void UnaryOperatorExpr::codegen(Code &code)
+{
+
+}
+
+void BinaryOperatorExpr::codegen(Code &code)
+{
+
+}
+
+void LambdaExpr::codegen(Code &code)
+{
+
+}
+
+void NewExpr::codegen(Code &code)
+{
+
+}
+
+void DotExpr::codegen(Code &code)
+{
+
+}
+
+void EnumExpr::codegen(Code &code)
+{
+
+}
+
+void MatchExpr::codegen(Code &code)
+{
+
+}
+
+void ListExpr::codegen(Code &code)
+{
+
+}
+
+void IndexExpr::codegen(Code &code)
+{
+
+}
+
+void DictExpr::codegen(Code &code)
+{
+
+}
+
+void UsingStmt::codegen(Code &code)
+{
+
+}
+
+void ExprStmt::codegen(Code &code)
+{
+
+}
+
+void VariableDeclarationStmt::codegen(Code &code)
+{
+
+}
+
+void VariableAssignStmt::codegen(Code &code)
+{
+
+}
+
+void FunctionDeclarationStmt::codegen(Code &code)
+{
+
+}
+
+void ClassDeclarationStmt::codegen(Code &code)
+{
+
+}
+
+void BreakStmt::codegen(Code &code)
+{
+
+}
+
+void ContinueStmt::codegen(Code &code)
+{
+
+}
+
+void ReturnStmt::codegen(Code &code)
+{
+
+}
+
+void IfElseStmt::codegen(Code &code)
+{
+
+}
+
+void WhileStmt::codegen(Code &code)
+{
+
+}
+
+void DoWhileStmt::codegen(Code &code)
+{
+
+}
+
+void ForStmt::codegen(Code &code)
+{
+
+}
+
+void ForeachStmt::codegen(Code &code)
+{
+
+}
 }
