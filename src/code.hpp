c464a56e746b1c4e41f18b859b8200d3a996977c
mu001999 <mu001999@outlook.com>
2019-11-24 21:48:01 +0800

    add interface

--- src/code.hpp
+++ src/code.hpp
@@ -14,6 +14,11 @@ class Code
         instructions_.emplace_back(std::move(ins));
     }
 
+    std::list<Instruction> &get_instructions()
+    {
+        return instructions_;
+    }
+
   private:
     std::list<Instruction> instructions_;
 };
