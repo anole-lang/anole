dfb9c46c2abbcc6beeee279ab8da13162b01adf1
mu001999 <mu001999@outlook.com>
2019-11-24 21:48:10 +0800

    update

--- src/vm.hpp
+++ src/vm.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
+#include "operation.hpp"
 #include <memory>
-#include "code.hpp"
 
 namespace ice_language
 {
+class Instruction;
+class Code;
+
 class VM
 {
   public:
     void execute_ins(Instruction &ins);
     void ececute_code(Code &code);
+
+  private:
+    void push(Instruction &ins);
 };
 } // namespace ice_language
