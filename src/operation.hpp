43a6af2722dac820f8dffe1e25799b0193f78a75
mu001999 <mu001999@outlook.com>
2019-11-25 23:12:31 +0800

    update operation

--- src/operation.hpp
+++ src/operation.hpp
@@ -9,20 +9,4 @@ enum class Opcode
     Push,
     Add
 };
-
-class OprandsBase {};
-
-template <typename ...Types>
-class Oprands : public OprandsBase
-{
-  public:
-    Oprands(Types ...args) : values_(args...) {}
-    std::tuple<Types> &get_values()
-    {
-        return values_;
-    }
-
-  private:
-    std::tuple<Types> values_;
-};
 }
