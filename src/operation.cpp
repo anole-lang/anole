c07c13b3950d4a88eb96f96d91cc6474c3c9b501
mu001999 <mu001999@outlook.com>
2019-11-26 12:53:12 +0800

    update new-operation

---
+++ src/operation.cpp
@@ -0,0 +1,29 @@
+#include "vm.hpp"
+#include "operation.hpp"
+
+using namespace std;
+
+namespace ice_language
+{
+void Push::execute(VM &vm)
+{
+    for (auto oprand : oprands_)
+    {
+        vm.push(oprand);
+    }
+}
+
+void Add::execute(VM &vm)
+{
+    auto lhs = *vm.pop<long>();
+    auto rhs = *vm.pop<long>();
+    vm.push(make_shared<long>(lhs + rhs));
+}
+
+void Add::execute(VM &vm)
+{
+    auto lhs = *vm.pop<long>();
+    auto rhs = *vm.pop<long>();
+    vm.push(make_shared<long>(lhs - rhs));
+}
+}
