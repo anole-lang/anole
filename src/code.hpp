f3c6ad0e458287bf7c32a45d9e931cea17bf9728
mu001999 <mu001999@outlook.com>
2019-12-01 20:21:46 +0800

    udpate code

--- src/code.hpp
+++ src/code.hpp
@@ -19,13 +19,16 @@ class Code
     template <typename Ins, typename T>
     void add_ins(T value)
     {
-        if constexpr (std::is_same<T, std::nullptr_t>::value)
+        if constexpr (std::is_same<Ins, Push>::value)
         {
-            instructions_.push_back(std::make_shared<Ins>(nullptr));
-        }
-        else if constexpr (std::is_same<Ins, Push>::value)
-        {
-            instructions_.push_back(std::make_shared<Push>(std::make_shared<T>(value)));
+            if constexpr (std::is_same<T, std::nullptr_t>::value)
+            {
+                instructions_.push_back(std::make_shared<Ins>(nullptr));
+            }
+            else
+            {
+                instructions_.push_back(std::make_shared<Push>(std::make_shared<T>(value)));
+            }
         }
         else
         {
