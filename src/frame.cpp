b816e98166b53d034e1914b84bb8d07040667314
mu001999 <mu001999@outlook.com>
2019-12-03 16:04:10 +0800

    update execute run

--- src/frame.cpp
+++ src/frame.cpp
@@ -27,6 +27,53 @@ void Frame::execute_code(Code &code)
             create_symbol(*OPRAND(string));
             break;
 
+        case Op::Load:
+            push(load_symbol(*OPRAND(string)));
+            break;
+
+        case Op::Store:
+            {
+                auto p = pop_straight();
+                *p = pop();
+            }
+            break;
+
+        case Op::Call:
+            // ... to complete
+            // the code of the definination
+            // should be contained by the function object
+            // code may be what here:
+            // auto func = scope->pop<FunctionObject>();
+            // func->call(pre_);
+            break;
+
+        case Op::Neg:
+            {
+                auto v = *pop<long>();
+                push(make_shared<long>(-v));
+            }
+            break;
+
+        case Op::Add:
+            {
+                auto lhs = *pop<long>();
+                auto rhs = *pop<long>();
+                push(make_shared<long>(lhs + rhs));
+            }
+            break;
+
+        case Op::Sub:
+            {
+                auto lhs = *pop<long>();
+                auto rhs = *pop<long>();
+                push(make_shared<long>(lhs - rhs));
+            }
+            break;
+
+        case Op::Return:
+            set_return();
+            break;
+
         default:
             break;
         }
