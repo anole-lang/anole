4485d5d7c9ee261b42503b7e7ae4da5728d1b01b
mu001999 <mu001999@outlook.com>
2019-12-05 12:22:29 +0800

    remove mis code

--- src/instruction.hpp
+++ src/instruction.hpp
@@ -5,134 +5,28 @@
 #include <utility>
 #include "helper.hpp"
 
-/*
-
-class Pop : public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Push : public Instruction
-{
-  public:
-    Push(Ptr<void> oprand)
-      : oprand_(oprand) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    Ptr<void> oprand_;
-};
-
-class Create : public Instruction
-{
-  public:
-    Create(std::string name)
-      : name_(std::move(name)) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    std::string name_;
-};
-
-class Load : public Instruction
-{
-  public:
-    Load(std::string name)
-      : name_(std::move(name)) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    std::string name_;
-};
-
-class Store : public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Call : public Instruction
-{
-  public:
-    Call(std::size_t num) : num_(num) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    std::size_t num_;
-};
-
-class Neg : public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Add: public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Sub: public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Return : public Instruction
-{
-  public:
-    void execute(Ptr<Frame> scope) override;
-};
-
-class Jump : public Instruction
-{
-  public:
-    Jump(std::size_t ind)
-      : ind_(ind) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    std::size_t ind_;
-};
-
-class JumpIfNot : public Instruction
-{
-  public:
-    JumpIfNot(std::size_t ind)
-      : ind_(ind) {}
-    void execute(Ptr<Frame> scope) override;
-
-  private:
-    std::size_t ind_;
-};
-*/
-
 namespace ice_language
 {
-class Frame;
-
 enum class Op
 {
     PlaceHolder,
 
-    Pop,          // ok
-    Push,         // ok
+    Pop,          // done
+    Push,         // done
 
-    Create,       // ok
-    Load,         // ok
-    Store,        // ok
+    Create,       // done
+    Load,         // done
+    Store,        // done
 
-    Neg,          // ok
-    Add,          // ok
-    Sub,          // ok
+    Neg,          // done
+    Add,          // done
+    Sub,          // done
 
-    ScopeBegin,
-    ScopeEnd,
+    ScopeBegin,   // done
+    ScopeEnd,     // done
     Call,
-    Return,       // ok
-    Jump,         // ok
+    Return,       // done
+    Jump,         // done
     JumpIf,
     JumpIfNot,
 };
