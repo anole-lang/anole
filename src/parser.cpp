d1370f73dcf5c84ee383946d1ffe4de403f3a606
Chen kai <ck770701780@hotmail.com>
2019-12-09 23:08:28 +0800

    Add code positioning to error info

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,7 +6,7 @@
 
 #define THROW(MESSAGE)                      throw runtime_error(MESSAGE)
 #define CHECK_AND_THROW(TOKEN_ID, MESSAGE)  if (current_token_.token_id != TOKEN_ID) \
-                                                THROW(MESSAGE)
+                                                THROW(get_location() + MESSAGE + "\033[0m")
 
 using namespace std;
 
@@ -767,6 +767,11 @@ Ptr<Expr> Parser::gen_list_expr()
     get_next_token(); // eat(']')
 
     return make_shared<ListExpr>(expressions);
+}
+
+std::string Parser::get_location()
+{
+    return tokenizer_.location();
 }
 }
 
--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -13,9 +13,15 @@ class Tokenizer
   public:
     Tokenizer(std::istream &in = std::cin);
     Token next();
+    std::string location();
 
   private:
     std::istream &input_stream_;
     char last_input_;
+    u_int32_t cur_line_num_;
+    u_int32_t cur_char_at_line_;
+    u_int32_t last_line_num_;
+    u_int32_t last_char_at_line_;
+    std::string cur_line_;
 };
 }
