8102aa6ee4933b5af460c57ae0799bfdcb828e47
mu001999 <mu001999@outlook.com>
2019-12-16 23:15:53 +0800

    update object

--- src/object.cpp
+++ src/object.cpp
@@ -7,8 +7,34 @@ using namespace std;
 namespace ice_language
 {
 Object::~Object() = default;
+
 bool Object::to_bool()
 {
     throw runtime_error("cannot translate to bool");
+}
+
+string Object::to_str()
+{
+    return "easy to string"s;
+}
+
+ObjectPtr Object::neg()
+{
+    throw runtime_error("no neg method");
+}
+
+ObjectPtr Object::add(ObjectPtr obj)
+{
+    throw runtime_error("no add method");
+}
+
+ObjectPtr Object::sub(ObjectPtr obj)
+{
+    throw runtime_error("no sub method");
+}
+
+ObjectPtr Object::mul(ObjectPtr obj)
+{
+    throw runtime_error("no mul method");
 }
 }
--- src/object.hpp
+++ src/object.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <string>
+#include "helper.hpp"
+
 namespace ice_language
 {
+using ObjectPtr = Ptr<class Object>;
+
 class Object
 {
   public:
     virtual ~Object() = 0;
     virtual bool to_bool();
+    virtual std::string to_str();
+    virtual ObjectPtr neg();
+    virtual ObjectPtr add(ObjectPtr);
+    virtual ObjectPtr sub(ObjectPtr);
+    virtual ObjectPtr mul(ObjectPtr);
 };
 }
