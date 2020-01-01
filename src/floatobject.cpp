a6ba132bb833d06c3538963f14d7cce0648c2a03
mu001999 <mu001999@outlook.com>
2020-01-01 22:42:53 +0800

    update floatobject

--- src/floatobject.cpp
+++ src/floatobject.cpp
@@ -1,4 +1,5 @@
 #include "floatobject.hpp"
+#include "boolobject.hpp"
 
 using namespace std;
 
@@ -7,5 +8,116 @@ namespace ice_language
 bool FloatObject::to_bool()
 {
     return value_;
+}
+
+string FloatObject::to_str()
+{
+    return to_string(value_);
+}
+
+string FloatObject::to_key()
+{
+    return 'f' + to_str();
+}
+
+ObjectPtr FloatObject::neg()
+{
+    return make_shared<FloatObject>(-value_);
+}
+
+ObjectPtr FloatObject::add(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return make_shared<FloatObject>(value_ + p->value_);
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::sub(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return make_shared<FloatObject>(value_ - p->value_);
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::mul(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return make_shared<FloatObject>(value_ * p->value_);
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::div(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return make_shared<FloatObject>(value_ / p->value_);
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::ceq(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return value_ == p->value_ ? theTrue : theFalse;
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::cne(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return value_ != p->value_ ? theTrue : theFalse;
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::clt(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return value_ < p->value_ ? theTrue : theFalse;
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
+}
+
+ObjectPtr FloatObject::cle(ObjectPtr obj)
+{
+    if (auto p = dynamic_pointer_cast<FloatObject>(obj))
+    {
+        return value_ <= p->value_ ? theTrue : theFalse;
+    }
+    else
+    {
+        throw runtime_error("no match method");
+    }
 }
 }
--- src/floatobject.hpp
+++ src/floatobject.hpp
@@ -10,6 +10,17 @@ class FloatObject : public Object
     FloatObject(double value) : value_(value) {}
 
     bool to_bool() override;
+    std::string to_str() override;
+    std::string to_key() override;
+    ObjectPtr neg() override;
+    ObjectPtr add(ObjectPtr) override;
+    ObjectPtr sub(ObjectPtr) override;
+    ObjectPtr mul(ObjectPtr) override;
+    ObjectPtr div(ObjectPtr) override;
+    ObjectPtr ceq(ObjectPtr) override;
+    ObjectPtr cne(ObjectPtr) override;
+    ObjectPtr clt(ObjectPtr) override;
+    ObjectPtr cle(ObjectPtr) override;
 
   private:
     double value_;
