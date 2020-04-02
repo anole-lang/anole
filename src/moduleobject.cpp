337125bdebd3aca700acea200f79c294016d6f74
mu001999 <mu001999@outlook.com>
2020-04-02 18:09:43 +0800

    add CppModuleObject and distinct from origin ModuleObject

--- src/moduleobject.cpp
+++ src/moduleobject.cpp
@@ -1,15 +1,42 @@
+#include <dlfcn.h>
 #include "moduleobject.hpp"
+#include "builtinfuncobject.hpp"
 
 using namespace std;
 
 namespace ice_language
 {
-Ptr<ObjectPtr> ModuleObject::load_member(const string &name)
+ModuleObject::~ModuleObject() = default;
+
+Ptr<ObjectPtr> IceModuleObject::load_member(const string &name)
 {
     if (scope_->symbols().count(name))
     {
         return scope_->load_symbol(name);
     }
     return Object::load_member(name);
+}
+
+CppModuleObject::CppModuleObject(const string &path)
+{
+    handle_ = dlopen(path.c_str(), RTLD_NOW);
+    good_ = (handle_ != NULL);
+}
+
+CppModuleObject::~CppModuleObject()
+{
+    dlclose(handle_);
+}
+
+Ptr<ObjectPtr> CppModuleObject::load_member(const string &name)
+{
+    using FuncType = void (*)();
+    auto func = reinterpret_cast<FuncType>(dlsym(handle_, name.c_str()));
+    if (!func)
+    {
+        throw runtime_error(dlerror());
+    }
+    auto result = make_shared<BuiltInFunctionObject>(func);
+    return make_shared<ObjectPtr>(result);
 }
 }
--- src/moduleobject.hpp
+++ src/moduleobject.hpp
@@ -8,11 +8,36 @@ namespace ice_language
 class ModuleObject : public Object
 {
   public:
-    ModuleObject(Ptr<Scope> scope)
+    ModuleObject() = default;
+    virtual ~ModuleObject() = 0;
+    virtual Ptr<ObjectPtr> load_member(const std::string &name) = 0;
+};
+
+class IceModuleObject : public ModuleObject
+{
+  public:
+    IceModuleObject(Ptr<Scope> scope)
       : scope_(scope) {}
     Ptr<ObjectPtr> load_member(const std::string &name) override;
 
   private:
     Ptr<Scope> scope_;
+};
+
+class CppModuleObject : public ModuleObject
+{
+  public:
+    CppModuleObject(const std::string &path);
+    ~CppModuleObject();
+    Ptr<ObjectPtr> load_member(const std::string &name) override;
+
+    bool good()
+    {
+        return good_;
+    }
+
+  private:
+    bool good_;
+    void *handle_;
 };
 }
