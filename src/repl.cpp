6221d101b3b6d6e6418d358e4e64ecbdbfbe67ed
mu001999 <mu001999@outlook.com>
2020-04-05 23:44:45 +0800

    Support history at REPL mode

--- src/repl.cpp
+++ src/repl.cpp
@@ -1,15 +1,49 @@
+#include <csetjmp>
 #include <sstream>
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "code.hpp"
 #include "repl.hpp"
 #include "parser.hpp"
+#include "context.hpp"
 #include "noneobject.hpp"
 
 using namespace std;
+
+namespace
+{
+static string read_line(const char * str)
+{
+    char *temp = readline(str);
+    if (!temp)
+    {
+        exit(0);
+    }
+    string line = temp;
+    free(temp);
+    return line;
+}
+
+static void handle_sigint(int)
+{
+    cout << "\b \b\b \b\nKeyboard Interrupt\n";
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
+}
 
 namespace ice_language
 {
 void replrun::run()
 {
+    signal(SIGINT, handle_sigint);
+    rl_bind_key('\t', rl_insert);
+    read_history(NULL);
+
     cout <<
 "    _____________________\n"
 "   /_  ___/ _____/ _____/\n"
@@ -20,27 +54,36 @@ void replrun::run()
     AST::interpretive() = true;
 
     string line;
-    while (line.empty())
-    {
-        cout << "\033[1m>>\033[0m ";
-        std::getline(cin, line);
-    }
-    istringstream ss(line += '\n');
-
+    istringstream ss;
     Parser parser{ss};
     auto code = make_shared<Code>();
     theCurrentContext = make_shared<Context>(code);
 
-    parser.set_continue_action([&ss, &line, &parser]
+    parser.set_continue_action([&ss, &parser]
     {
-        cout << "\033[1m..\033[0m ";
-        std::getline(cin, line);
-        ss.clear(); ss.str(line.empty() ? line : (line += '\n'));
+        auto line = read_line(".. ");
+        if (!line.empty())
+        {
+            add_history(line.c_str());
+            write_history(line.c_str());
+        }
+        ss.clear();
+        ss.str(line += '\n');
         parser.cont();
     });
 
     while (true)
     {
+        do
+        {
+            line = read_line(">> ");
+        } while (line.empty());
+        add_history(line.c_str());
+        write_history(line.c_str());
+        ss.clear();
+        ss.str(line + '\n');
+        parser.reset();
+
         try
         {
             theCurrentContext->pc() = code->size();
@@ -65,14 +108,6 @@ void replrun::run()
         {
             cerr << e.what() << endl;
         }
-
-        do
-        {
-            cout << "\033[1m>>\033[0m ";
-            std::getline(cin, line);
-        } while (line.empty());
-        ss.clear(); ss.str(line += '\n');
-        parser.reset();
     }
 }
 }
