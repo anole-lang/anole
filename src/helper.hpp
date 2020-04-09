9eea8d3e1475db2a2584b1c1ebf5f20b5170133c
mu001999 <mu001999@outlook.com>
2020-04-09 23:24:35 +0800

    add namesapce info for different output

--- src/helper.hpp
+++ src/helper.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 namespace ice_language
 {
 template <typename T>
 using Ptr = std::shared_ptr<T>;
+
+namespace info
+{
+inline std::string strong(const std::string &str)
+{
+    return "\033[1m" + str + "\033[0m";
+}
+
+inline std::string warning(const std::string &str)
+{
+    return "\033[31m" + str + "\033[0m";
+}
+}
 }
