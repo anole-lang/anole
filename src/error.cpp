77987a03f03d6a0c9c312c52362a94da239d73fa
mu001999 <mu001999@outlook.com>
2020-04-09 17:43:24 +0800

    fix bug

--- src/error.cpp
+++ src/error.cpp
@@ -1,10 +1,45 @@
 #include "error.hpp"
+#include "context.hpp"
+
+using namespace std;
 
 namespace ice_language
 {
+CompileError::CompileError(string err)
+  : err_(err) {}
+
+CompileError
+&CompileError::operator=(const CompileError &other) noexcept
+{
+    err_ = other.err_;
+    return *this;
+}
+
 const char *CompileError::what() const noexcept
 {
     return err_.c_str();
+}
+
+RuntimeError::RuntimeError(const string &err)
+{
+    err_ = "\033[1mrunning at "
+        + theCurrentContext->code()->from();
+    auto &mapping = theCurrentContext->code()->mapping();
+    if (mapping.count(theCurrentContext->pc()))
+    {
+        auto pos = mapping[theCurrentContext->pc()];
+        err_ += ":" + to_string(pos.first)
+            + ":" + to_string(pos.second)
+            + ": \033[31merror: ";
+    }
+    err_ += err + "\033[0m";
+}
+
+RuntimeError
+&RuntimeError::operator=(const RuntimeError &other) noexcept
+{
+    err_ = other.err_;
+    return *this;
 }
 
 const char *RuntimeError::what() const noexcept
--- src/error.hpp
+++ src/error.hpp
@@ -2,21 +2,14 @@
 
 #include <string>
 #include <exception>
-#include "context.hpp"
 
 namespace ice_language
 {
 class CompileError : public std::exception
 {
   public:
-    CompileError(std::string err)
-      : err_(std::move(err)) {}
-
-    CompileError &operator=(const CompileError &other) noexcept
-    {
-        err_ = other.err_;
-        return *this;
-    }
+    CompileError(std::string err);
+    CompileError &operator=(const CompileError &other) noexcept;
 
     const char *what() const noexcept override;
 
@@ -27,26 +20,8 @@ class CompileError : public std::exception
 class RuntimeError : public std::exception
 {
   public:
-    RuntimeError(const std::string &err)
-    {
-        err_ = "\033[1mrunning at "
-            + theCurrentContext->code()->from();
-        auto &mapping = theCurrentContext->code()->mapping();
-        if (mapping.count(theCurrentContext->pc()))
-        {
-            auto pos = mapping[theCurrentContext->pc()];
-            err_ += ":" + to_string(pos.first)
-                + ":" + to_string(pos.second)
-                + ": \033[31merror: ";
-        }
-        err_ += err + "\033[0m";
-    }
-
-    RuntimeError &operator=(const RuntimeError &other) noexcept
-    {
-        err_ = other.err_;
-        return *this;
-    }
+    RuntimeError(const std::string &err);
+    RuntimeError &operator=(const RuntimeError &other) noexcept;
 
     const char *what() const noexcept override;
 
