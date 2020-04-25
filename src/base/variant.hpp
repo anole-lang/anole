e8aac9bb52eaee29f8adc18a5a53edb8662de1c1
mu001999 <mu001999@outlook.com>
2020-04-25 23:27:44 +0800

    update

--- src/base/variant.hpp
+++ src/base/variant.hpp
@@ -2,6 +2,16 @@
 
 namespace base
 {
-template<typename ...Ts>
-class variant;
+template<typename ...Types>
+class variant
+{
+  public:
+    constexpr variant() noexcept;
+    constexpr variant(const variant &other);
+    constexpr variant(variant &&other) noexcept;
+    template<typename T>
+    constexpr variant(T &&t) noexcept;
+
+  private:
+};
 }
