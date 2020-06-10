5b5db619fa989f8599682f1db442e256c2bf480d
mu001999 <mu001999@outlook.com>
2020-06-10 21:52:28 +0800

    [STORE COMPUTED VALUE]

--- src/thunkobject.hpp
+++ src/thunkobject.hpp
@@ -12,14 +12,24 @@ class ThunkObject : public Object
     ThunkObject(SPtr<Scope> pre_scope,
         SPtr<Code> code, std::size_t base)
       : Object(ObjectType::Thunk)
+      , computed_(false)
       , scope_(std::make_shared<Scope>(pre_scope))
       , code_(code), base_(base) {}
 
+    void set_result(Address res)
+    {
+        result_ = res;
+        computed_ = true;
+    }
+    bool computed() { return computed_; }
+    Address result() { return result_; }
     SPtr<Scope> scope() { return scope_; }
     SPtr<Code> code() { return code_; }
     std::size_t base() { return base_; }
 
   private:
+    bool computed_;
+    Address result_;
     SPtr<Scope> scope_;
     SPtr<Code> code_;
     std::size_t base_;
