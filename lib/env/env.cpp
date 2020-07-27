4e908347892689d0817b666851c967244cc43d57
mu001999 <mu001999@outlook.com>
2020-07-27 20:18:07 +0800

    [LIB] add lib env to get the command-line arguments

---
+++ lib/env/env.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include "../../src/context.hpp"
+#include "../../src/listobject.hpp"
+#include "../../src/stringobject.hpp"
+
+using namespace std;
+using namespace anole;
+
+extern "C"
+{
+extern vector<string> _FUNCTIONS;
+void __args(size_t);
+}
+
+vector<string> _FUNCTIONS
+{
+    "__args"s
+};
+
+void __args(size_t n)
+{
+    if (n != 0)
+    {
+        throw RuntimeError("args need no arguments");
+    }
+
+    auto raw_args = Context::get_args();
+
+    auto args = make_shared<ListObject>();
+    for (auto arg : raw_args)
+    {
+        args->append(make_shared<StringObject>(string(arg)));
+    }
+
+    theCurrentContext->push(args);
+}
