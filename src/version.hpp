184ffbd78a8cb59d6c426d81431ac8d970fbc0f7
mu001999 <mu001999@outlook.com>
2020-08-07 15:45:49 +0800

    [VERSION] add version info

--- src/version.hpp
+++ src/version.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <cstdint>
+#include "base.hpp"
 
 namespace anole
 {
+using Magic = Size;
 
+constexpr auto theVersion = "HEAD 0.0.17 2020/08/07";
+constexpr Magic theMagic = 2020'08'07;
 }
