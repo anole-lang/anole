f7b0f9d2b4cc4b26527f8498cedf77343f7503a5
mu001999 <mu001999@outlook.com>
2020-08-18 19:21:58 +0800

    [CODE] update collector

--- src/collector.hpp
+++ src/collector.hpp
@@ -2,9 +2,21 @@
 
 namespace anole
 {
+/**
+ * Collector will find objects/scopes/contexts
+ *  which are referenced and then deallocate others
+*/
 class Collector
 {
   public:
     Collector() = default;
+
+    /**
+     * record allocated address
+     *
+     * void record(Address);
+    */
+
+    void gc();
 };
 } // namespace anole
