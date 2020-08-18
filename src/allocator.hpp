953264f00f8b3763bb7a883cc985592e9c66acc1
mu001999 <mu001999@outlook.com>
2020-08-18 19:41:02 +0800

    [CODE] add comments

--- src/allocator.hpp
+++ src/allocator.hpp
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <list>
+
 namespace anole
 {
+/**
+ * by allocator,
+ *  we can allocate memories for variables
+ *  and every variable could reference to one object
+ *
+ * objects are also allocated by allocator
+ *  and then bind variables with objects by var.bind()
+*/
 class Allocator
 {
   public:
     Allocator() = default;
+
+  private:
 };
 } // namespace anole
