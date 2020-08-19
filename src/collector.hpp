a9e9c4ce4b9a70458c3b0049f85d9a82726ee1d9
mu001999 <mu001999@outlook.com>
2020-08-20 00:58:53 +0800

    [CODE] update Collector

--- src/collector.hpp
+++ src/collector.hpp
@@ -1,22 +1,69 @@
 #pragma once
 
+#include <set>
+
 namespace anole
 {
+template<typename T>
+class EnableCollect
+{
+  public:
+    EnableCollect(T *bind)
+      : bind_(bind)
+    {
+        // ...
+    }
+
+    T *binded()
+    {
+        return bind_;
+    }
+
+  private:
+    T *bind_;
+};
+
 /**
- * Collector will find objects/scopes/contexts
+ * Collector will find variables/objects/scopes/contexts/stacks
  *  which are referenced and then deallocate others
 */
 class Collector
 {
   public:
+    static Collector &collector();
+
+  public:
     Collector() = default;
 
     /**
      * record allocated address
+     *  and only recorded address could be deallocated
      *
-     * void record(Address);
+     * void record(address);
     */
+    template<typename T>
+    void record(T *ptr)
+    {
+        recorded<T>().insert(ptr);
+    }
 
-    void gc();
+    void gc()
+    {
+        // ...
+    }
+
+  private:
+    template<typename T>
+    std::set<T *> &recorded()
+    {
+        static std::set<T *> rcod;
+        return rcod;
+    }
 };
+
+inline Collector &Collector::collector()
+{
+    static Collector clctor;
+    return clctor;
+}
 } // namespace anole
