05f9c0d4076fae387d56c156e4bcdbdcf41442dd
mu001999 <mu001999@outlook.com>
2020-08-19 23:43:31 +0800

    [CODE] update Variable

--- src/variable.hpp
+++ src/variable.hpp
@@ -2,9 +2,34 @@
 
 namespace anole
 {
+class Object;
+
 class Variable
 {
   public:
-    Variable() = default;
+    using Pointer = Object *;
+
+    Variable() : obj_(nullptr) {}
+    Variable(Pointer obj) : obj_(obj) {}
+
+    void bind(Pointer obj)
+    {
+        obj_ = obj;
+    }
+
+    Pointer obj()
+    {
+        return obj_;
+    }
+
+    operator bool()
+    {
+        return obj_ != nullptr;
+    }
+
+  private:
+    Pointer obj_;
 };
+
+using Address = Variable *;
 } // namespace anole
