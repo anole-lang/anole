ce84308377bcdd3eb441f289139760ae255d469c
mu001999 <mu001999@outlook.com>
2020-08-20 13:39:54 +0800

    [GC] update gc

--- src/collector.cpp
+++ src/collector.cpp
@@ -1,5 +1,6 @@
 #include "context.hpp"
 #include "collector.hpp"
+#include "../light/type_traits.hpp"
 
 using namespace std;
 
@@ -8,6 +9,21 @@ namespace anole
 // TODO
 void Collector::gc()
 {
-    // ...
+    auto &mkd_vars = marked<Variable>();
+    light::remove_reference_t<decltype(mkd_vars)> collected;
+
+    /**
+     * collect variables from Context::current()
+    */
+
+    for (auto &addr : mkd_vars)
+    {
+        // if cannot visit variable the variable
+        if (!collected.count(addr))
+        {
+            mkd_vars.erase(addr);
+            Allocator<Variable>::dealloc(addr);
+        }
+    }
 }
 }
