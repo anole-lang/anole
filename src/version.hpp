6107467355aeb087ec39b56591e05cd8ee99f9ad
mu001999 <mu001999@outlook.com>
2020-08-21 19:10:48 +0800

    0.0.18 release

--- src/version.hpp
+++ src/version.hpp
@@ -17,7 +17,7 @@ namespace anole
 */
 struct Version
 {
-    constexpr static auto literal = "0.0.18 2020/08/08";
+    constexpr static auto literal = "0.0.18 2020/08/20";
 
     constexpr static Size major    = 0;
     constexpr static Size minor    = 0;
